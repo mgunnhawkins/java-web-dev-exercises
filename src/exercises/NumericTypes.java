0fd325a3ebaa26c1b53a290c43508d510acbb098
Melanie Gunn <melanie@melaniegunn.com>
2021-07-13 14:43:53 -0500

    exercises finished

---
+++ src/exercises/NumericTypes.java
@@ -0,0 +1,21 @@
+package exercises;
+import java.util.Scanner;
+public class NumericTypes {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles have you driven?");
+        Double milesDriven = input.nextDouble();
+
+        System.out.println("How many gallons have you consumed?");
+        Double gallonsUsed = input.nextDouble();
+
+        Double mpg = milesDriven / gallonsUsed;
+        System.out.println("Your car has approximately " + mpg + " mpg.");
+
+
+
+
+        }
+
+}
+
