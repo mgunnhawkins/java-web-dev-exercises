467c4262298f77d2f06bd475c8955af26b52fcef
Melanie Gunn <mgunn589@gmail.com>
2021-08-24 09:06:29 -0500

    update

--- src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class ConeComparator implements Comparator<Cone>{
     @Override
-    public int compare(Cone cone1, Cone cone2) {
+    public int compare(Cone cone1, Cone cone2) {//example...comparing people by height
         if(cone1.getCost() - (cone2.getCost()) < 0){
             return -1;
         } else if (cone1.getCost() - (cone2.getCost()) > 0){
